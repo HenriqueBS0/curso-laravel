ebd468e8b5163819b2a7fd472e4a52725e82c530
HenriqueBS0 <Henrique.10agr@gmail.com>
2021-01-26 09:40:36 -0300

    arquivos estaticos

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
     </head>
     <body class="antialiased">
       <h1>Algum Titulo</h1>
+      <img src="/img/banner.jpg" alt="Banner">
       @if (10 > 5)
           <p>A condição é true</p>
       @endif
